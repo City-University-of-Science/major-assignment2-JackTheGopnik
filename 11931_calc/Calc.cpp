c226264f910b664f9215822e2e8a570754eacc95
Arsalan Fazal <arsalanfazal11931@gmail.com>
2022-12-06 16:30:33 +0000

    Class added.

--- 11931_calc/Calc.cpp
+++ 11931_calc/Calc.cpp
@@ -1,200 +1,215 @@
 #include <iostream>
 #include <math.h>
 
-double add (double x, double y)
+class math_calculator_scientific {
+double x, y; 
+public:
+
+void value_setter()
+{
+double xc284, xc285; //actual ommitted addresses of where the values are stored in memory :P    
+std::cout<<"enter the first number:"<<std::endl;
+std::cin>>xc284;
+std::cout<<"enter the second number:"<<std::endl;
+std::cin>>xc285;
+x = xc284;
+y = xc285;
+
+}
+
+double add ()
 {
     return x + y;
 }
 
-double subtract (double x, double y)
+double subtract ()
 {
     return x - y;
 }
 
-double multiply (double x, double y)
+double multiply ()
 {
     return x * y;
 }
 
-double divide (double x, double y)
+double divide ()
 {
     if(y==0)
     {
-        std::cout<<"You Entered a Null value as the dividand, Undefined.\n";
-        return 0;
+     std::cout<<"You Entered a Null value as the dividand, Undefined.\n";
+
     }
     return x / y;
 }
 
-double sqroot (double x)
+double sqroot ()
 {
     return sqrt(x);
 }
 
-double log_simple (double x)
+double log_simple ()
 {
     return log(x);
 }
 
-double log_base (double x)
+double log_base ()
 {
     return log10(x);
 }
 
-double sine (double x)
+double sine ()
 {
     return sin(x);
 }
 
-double cosine (double x)
+double cosine ()
 {
     return cos(x);
 }
 
-double tangent (double x)
+double tangent ()
 {
     return tan(x);
 }
 
-double arc_sine (double x)
+double arc_sine ()
 {
     return asin(x);
 }
 
-double arc_cosine (double x)
+double arc_cosine ()
 {
     return acos(x);
 }
 
-double arc_tan (double x)
+double arc_tan ()
 {
     return atan(x);
 }
 
-double sineh (double x)
+double sineh ()
 {
     return sinh(x);
 }
 
-double cosineh (double x)
+double cosineh ()
 {
     return cosh(x);
 }
 
-double tangenth (double x)
+double tangenth ()
 {
     return tanh(x);
 }
 
-double cube (double x)
+double cube ()
 {
     return x*x*x;
 }
 
-double power (double x, double y)
+double power ()
 {
     return pow(x,y);
 }
 
-double average (double x, double y)
+double average ()
 {
     return (x+y)/2;
 }
 
+};
+
 int main()
 {
-double x, y, result; 
 
+math_calculator_scientific cal;
 int operation;
-
-std::cout<<"enter the first number:"<<std::endl;
-std::cin>>x;std::cout<<""<<std::endl;
-std::cout<<"enter the second number:"<<std::endl;
-std::cin>>y;std::cout<<""<<std::endl;
 std::cout<<"enter the operation you would like to perform:\n1 for addition , 2 for subtraction,3 for multiplication, 4 for division,";
 std::cout<<"1 for square root, 2 for natural log, 3 for log to the base 10,";
-std::cout<<"\n4 for sin , 5 for cos, 6 for tan,";
-std::cout<<"\n7 for asin(arcsine), 8 for acos(arccosine), 9 for atan(arctangent),";
-std::cout<<"\n10 for sinh(sine hyperbolic), 11 for cosh(cos hyperbolic), 12 tanh(tan hyperbolic)";
-std::cout<<"\n 13 for cube, 14 for average, 15 power";
+std::cout<<"\n5 for sin , 6 for cos, 7 for tan,";
+std::cout<<"\n8 for asin(arcsine), 9 for acos(arccosine), 10 for atan(arctangent),";
+std::cout<<"\n11 for sinh(sine hyperbolic), 12 for cosh(cos hyperbolic), 13 tanh(tan hyperbolic)";
+std::cout<<"\n 14 for cube, 15 for average, 16 power\n";
+
 
 do{
-
+std::cout<<"Your Choice: ";std::cin>>operation;    
 switch(operation)
 {
-case '1':
-    result = add(x,y);
-    std::cout<<"The Added Numbers"<<x<<" & "<<y<<"result in: "<<result<<std::endl;
+    double result;
+case 1:
+    result = cal.add();
     break;
 
-case '2':
-    result = subtract(x,y);
-    std::cout<<"The subtracted numbers"<<x<<" & "<<y<<"leave behind: "<<result<<std::endl;
+case 2:
+    result = cal.subtract();
     break;
 
-case '3':
-    result = multiply(x,y);
-    std::cout<<"The multiplied Numbers"<<x<<" & "<<y<<" result in: "<<result<<std::endl;
+case 3:
+    result = cal.multiply();
     break;
 
-case '4':
-    result = divide(x,y);
-    std::cout<<"The Divided Numbers"<<x<<" & "<<y<<"result in: "<<result<<std::endl;
+case 4:
+    result = cal.divide();
     break;
 
-case '5':
-    result = sine(x);
-    std::cout<<"The sine of"<<x<<"results in: "<<result<<std::endl;
+case 5:
+    result = cal.sine();
+
     break;
 
-case '6':
-    result = cos(x);
-    std::cout<<"The cos of "<<x<<" result in: "<<result<<std::endl;
+case 6:
+    result = cal.cosine();
+ 
     break;
 
-case '7':
-    result = add(x,y);
-    std::cout<<"The Added Numbers"<<x<<" & "<<y<<"result in: "<<result<<std::endl;
+case 7:
+    result = cal.tangent();
+
     break;
 
-case '8':
-    result = add(x,y);
-    std::cout<<"The Added Numbers"<<x<<" & "<<y<<"result in: "<<result<<std::endl;
+case 8:
+    result = cal.arc_sine();
+
     break;
 
-case '9':
-    result = add(x,y);
-    std::cout<<"The Added Numbers"<<x<<" & "<<y<<"result in: "<<result<<std::endl;
+case 9:
+    result = cal.arc_cosine();
+
     break;
 
-case '10':
-    result = add(x,y);
-    std::cout<<"The Added Numbers"<<x<<" & "<<y<<"result in: "<<result<<std::endl;
+case 10:
+    result = cal.arc_tan();
+
     break;
 
-case '11':
-    result = add(x,y);
-    std::cout<<"The Added Numbers"<<x<<" & "<<y<<"result in: "<<result<<std::endl;
+case 11:
+    result = cal.sineh();
+
     break;
 
-case '12':
-    result = add(x,y);
-    std::cout<<"The Added Numbers"<<x<<" & "<<y<<"result in: "<<result<<std::endl;
+case 12:
+    result = cal.cosineh();
+
     break;
 
-case '13':
-    result = add(x,y);
-    std::cout<<"The Added Numbers"<<x<<" & "<<y<<"result in: "<<result<<std::endl;
+case 13:
+    result = cal.tangenth();
+
     break;
 
-case '14':
-    result = add(x,y);
-    std::cout<<"The Added Numbers"<<x<<" & "<<y<<"result in: "<<result<<std::endl;
+case 14:
+    result = cal.cube();
+
     break;
 
-case '15':
-    result = add(x,y);
-    std::cout<<"The Added Numbers"<<x<<" & "<<y<<"result in: "<<result<<std::endl;
+case 15:
+    result = cal.average();
+ 
+    break;
+
+case 16:
+    result = cal.power();
     break;
 
 
@@ -204,7 +219,7 @@ case '15':
 
 
 
-}while(operation != 'e');
+}while(operation != 0);
 
 return 0;
 
